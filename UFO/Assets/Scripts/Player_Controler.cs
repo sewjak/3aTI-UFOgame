d1ad4c7b66729883329b5ebf882176205f0ef3e8
sewjak <sewjak12@gmail.com>
2023-02-14 09:31:44 +0100

    Added movement
    Added movement script

---
+++ UFO/Assets/Scripts/Player_Controler.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Controler : MonoBehaviour
+{
+    Rigidbody2D rigidbody2D;
+    void Start()
+    {
+        rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        Vector2 movement = new Vector2(moveHorizontal, moveVertical);
+        rigidbody2D.AddForce(movement);
+
+    }
+}
