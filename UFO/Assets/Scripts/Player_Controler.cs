a5310c5d2e7b1c1ab1bb272c8ee9fbc5dd512764
sewjak <sewjak12@gmail.com>
2023-03-21 10:25:02 +0100

    Added score

--- UFO/Assets/Scripts/Player_Controler.cs
+++ UFO/Assets/Scripts/Player_Controler.cs
@@ -1,14 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Player_Controler : MonoBehaviour
 {
     Rigidbody2D rigidbody2D;
     public float speed;
+    int Score;
+    public Text scoreText;
     void Start()
     {
         rigidbody2D = GetComponent<Rigidbody2D>();
+        Score = 1;
     }
 
     void Update()
@@ -23,5 +27,25 @@ public class Player_Controler : MonoBehaviour
         Vector2 movement = new Vector2(moveHorizontal, moveVertical);
         rigidbody2D.AddForce(movement*speed);
 
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Pickup"))
+        {
+            Destroy(collision.gameObject);
+            Score++;
+            UpdateScore();
+        }
+    }
+
+    void UpdateScore()
+    {
+        scoreText.text = $"Score: {Score.ToString()}";
+        if (true)
+        {
+
+        }
+        //winText.gameObject.setActivve(true);
     }
 }
