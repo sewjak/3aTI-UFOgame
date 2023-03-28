9d0e92e708ec1c5749550a06743006aa8effa18f
Konrad.Seweryn <sewjak12@gmail.com>
2023-03-28 10:10:00 +0200

    Added win
    Added win text disapper and apper

--- UFO/Assets/Scripts/Player_Controler.cs
+++ UFO/Assets/Scripts/Player_Controler.cs
@@ -5,19 +5,24 @@ using UnityEngine.UI;
 
 public class Player_Controler : MonoBehaviour
 {
-    Rigidbody2D rigidbody2D;
+    Rigidbody2D rd2D;
     public float speed;
     int Score;
     public Text scoreText;
+    public Text winText;
     void Start()
     {
-        rigidbody2D = GetComponent<Rigidbody2D>();
+        rd2D = GetComponent<Rigidbody2D>();
         Score = 0;
+        winText.enabled = false;
     }
 
     void Update()
     {
-        
+        if (Score == 7)
+        {
+            UpdateWin();
+        }
     }
 
     private void FixedUpdate()
@@ -25,7 +30,7 @@ public class Player_Controler : MonoBehaviour
         float moveHorizontal = Input.GetAxis("Horizontal");
         float moveVertical = Input.GetAxis("Vertical");
         Vector2 movement = new Vector2(moveHorizontal, moveVertical);
-        rigidbody2D.AddForce(movement*speed);
+        rd2D.AddForce(movement*speed);
 
     }
 
@@ -42,10 +47,12 @@ public class Player_Controler : MonoBehaviour
     void UpdateScore()
     {
         scoreText.text = $"Score: {Score.ToString()}";
-        if (true)
-        {
 
-        }
-        //winText.gameObject.setActivve(true);
+    }
+
+    void UpdateWin()
+    {
+        winText.enabled = true;
+        scoreText.enabled = false;
     }
 }
